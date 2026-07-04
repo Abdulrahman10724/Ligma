1d20813641cbc7fe5d4997ef2e2c94e215030a90
Abdul Rahman <k252508@nu.edu.pk>
2026-07-04 19:18:18 +0500

    folder structure

---
+++ Ligma_frontend/Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline bg-blue-500 text-white p-4 mx-auto px-2">welcome to frontend</h1>
+    </>
+  )
+}
+
+export default App
